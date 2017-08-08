88cec8e009165fcd3e9e6bb481ec88b648815016
Judzhin Miles <info@msbios.com>
2017-08-09 00:41:12 +0300

    Lazy Composer Commiter

--- themes/examples/theme.config.php
+++ themes/examples/theme.config.php
@@ -7,41 +7,36 @@ return [
     'identifier' => 'examples',
     'title' => 'Default Examples Theme',
     'description' => 'Default Examples Theme Descritpion',
-    // Doctype with which to seed the Doctype helper
-    // 'doctype' => $doctypeHelperConstantString, // e.g. HTML5, XHTML1
-    // TemplateMapResolver configuration
-    // template/path pairs
     'template_map' => [
-        // key => value
+        'somename/somename' => __DIR__ . '/view/path/to/file'
     ],
-    // TemplatePathStack configuration
-    // module/view script path pairs
     'template_path_stack' => [
-        __DIR__ . '//view',
+        __DIR__ . '/view/',
     ],
-    // Default suffix to use when resolving template scripts, if none, 'phtml' is used
-    // 'default_template_suffix' => $templateSuffix, // e.g. 'php'
-    // Controller namespace to template map
-    // or whitelisting for controller FQCN to template mapping
-    'controller_map' => [
-    ],
-    // Layout template name
-    // 'layout' => $layoutTemplateName, // e.g. 'layout/layout'
-    // ExceptionStrategy configuration
-    // 'display_exceptions' => $bool, // display exceptions in template
-    // 'exception_template' => $stringTemplateName, // e.g. 'error'
-    // RouteNotFoundStrategy configuration
-    // 'display_not_found_reason' => $bool, // display 404 reason in template
-    // 'not_found_template' => $stringTemplateName, // e.g. '404'
-    // Additional strategies to attach
-    // These should be class names or service names of View strategy classes
-    // that act as ListenerAggregates. They will be attached at priority 100,
-    // in the order registered.
     'translation_file_patterns' => [
         [
             'type' => 'gettext',
             'base_dir' => __DIR__ . '/language/',
             'pattern' => '%s.mo',
+        ],
+    ],
+    'assetic_manager' => [
+
+        'collections' => [
+        ],
+
+        'paths' => [
+        ],
+
+        'maps' => [
+        ],
+    ],
+    'widget_manager' => [
+        'template_map' => [
+            'somename/somename' => __DIR__ . '/view/path/to/file'
+        ],
+        'template_path_stack' => [
+            __DIR__ . '/view/',
         ],
     ],
 ];
