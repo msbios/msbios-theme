d6733baf8a9210cab1feb7d6833981eb0f7f0c41
Judzhin Miles <info@msbios.com>
2017-08-21 14:58:58 +0300

    Lazy Composer Commiter

---
+++ themes/default/theme.config.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+return [
+    'identifier' => 'default',
+    'title' => 'Default Theme Title',
+    'description' => 'Default Theme Description',
+
+    'template_map' => [
+        // Template Map
+    ],
+
+    'template_path_stack' => [
+        __DIR__ . '/view/',
+    ],
+
+    'translation_file_patterns' => [
+        [
+            'type' => 'gettext',
+            'base_dir' => __DIR__ . '/language/',
+            'pattern' => '%s.mo',
+        ],
+    ],
+
+    'widget_manager' => [
+        'template_map' => [
+        ],
+        'template_path_stack' => [
+            __DIR__ . '/widget/'
+        ],
+    ],
+];
