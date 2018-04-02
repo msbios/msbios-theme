8ec296715549c9ee6a8d11dad153d4d7f06c316d
Judzhin Miles <info@msbios.com>
2018-04-02 20:28:15 +0300

    Lazy Composer Commiter

--- src/Module.php
+++ src/Module.php
@@ -28,7 +28,7 @@ class Module implements
     AutoloaderProviderInterface
 {
     /** @const VERSION */
-    const VERSION = '1.0.10';
+    const VERSION = '1.0.11';
 
     /**
      * @return array
@@ -59,7 +59,7 @@ class Module implements
             $serviceManager
         ))->attach($target->getEventManager());
 
-        $serviceManager->get('ThemeManager')->loadThemes();
+        // $serviceManager->get('ThemeManager')->loadThemes();
     }
 
     /**
